509a6b5694b669b03ded5600162de49c8acc9067
gscahhh <73086275+gscahhh@users.noreply.github.com>
2022-02-18 16:00:10 +0800

    Add files via upload

--- simpleEA.m
+++ simpleEA.m
@@ -22,16 +22,17 @@ nbGen = 0; % generation counter
 nbEval = 0; % evaluation counter
 bestSoFarFit = 0; % best-so-far fitness value
 bestSoFarSolution = NaN; % best-so-far solution
+p_size=4
 %recorders
-fitness_gen=[]; % record the best fitness so far
+fitness_gen=[0]; % record the best fitness so far
 solution_gen=[];% record the best phenotype of each generation
 fitness_pop=[];% record the best fitness in current population 
 %% Below starting your code
 
 % Initialise a population
 %% TODO
-
-
+P=fix(rand(1,p_size)*31);
+B=dec2bin(P);
 % Evaluate the initial population
 %% TODO
 
@@ -39,13 +40,56 @@ fitness_pop=[];% record the best fitness in current population
 while (nbEval<T) 
 % Reproduction (selection, crossver)
 %% TODO
+%selection
+%eval
+evals=objective(P);
+nbEval=nbEval+p_size
+prob=evals/sum(evals);
+probs=prob
+len_gen=5
+for i=2:p_size
+    probs(i)=prob(i)+probs(i-1);
+end
+parents=[];
+for i=1:2
+    p=rand(1);
+    probs_aux=[probs,p];
+    idx=find(sort(probs_aux)==p);
+    parents=[parents P(idx)];
+end
+parents_B=dec2bin(parents);
+
+%crossover
+crossover_idx=fix(rand(1,1)*len_gen)+1;
+offs1=[parents_B(1,1:crossover_idx),parents_B(2,crossover_idx+1:len_gen)]
+offs2=[parents_B(2,1:crossover_idx),parents_B(1,crossover_idx+1:len_gen)]
 
 % Mutation
+mu_idx=fix(rand(1,1)*len_gen)+1;
+offs1(mu_idx)=num2str(1-str2num(offs1(mu_idx)));
+mu_idx=fix(rand(1,1)*len_gen)+1;
+offs2(mu_idx)=num2str(1-str2num(offs2(mu_idx)));
+offs1=bin2dec(offs1);
+offs2=bin2dec(offs2);
+P_next=[P offs1 offs2];
+evals=objective(P_next);
+P_next=[P_next;evals]'
+P_next=sortrows(P_next,-2) 
+P=P_next(1:p_size,1)
+
+x=1
+
 %% TODO
-
-
-bestSoFarFit
-bestSoFarSolution
+solution_gen=[solution_gen P_next(1,1)];% record the best phenotype of each generation
+fitness_pop=[fitness_pop P_next(1,2)]
+if P_next(1,2)>fitness_gen(length(fitness_gen))
+    bestSoFarFit=[fitness_gen P_next(1,2)]
+else
+    fitness_gen=[fitness_gen fitness_gen(fitness_gen(length(fitness_gen)))]
+end
+nbEval=nbEval+p_size+2
+bestSoFarFit=
+end
 
 
 
